0471de587375e87d70e3e9c46825c973e61ea67a
ajioka-fumito <ajioka.ds@gmail.com>
2020-03-11 16:37:10 +0900

    Initial Commit

---
+++ Convenience/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Convenience
+//
+//  Created by Ajioka Fumito on 2020/03/11.
+//  Copyright © 2020 Ajioka Fumito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
